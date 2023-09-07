f54fe784bca02ec5fd1a247620b4b4cd7bb3038b
Tosinfamzy <famurewatosin@gmail.com>
2023-09-07 18:16:17 +0100

    cleanup errors in the fetch and restructure return for load options

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -5,25 +5,31 @@ import { geoApiOptions } from '../../utils/api';
 export default function Search({ onSearchChange }) {
 	const [search, setSearch] = useState('');
 
-	const handleOnChange = (searchData) => {
-		setSearch(searchData);
-		onSearchChange(searchData);
-	};
-
 	const loadOptions = async (inputValue) => {
 		try {
 			const response = await fetch(
 				`${
 					import.meta.env.VITE_GEODB_API_URL
-				}/cities?minPopulation=100000namePrefix=${inputValue}`,
+				}/cities?minPopulation=100000&namePrefix=${inputValue}`,
 				geoApiOptions
 			);
 			const result = await response.json();
-			console.log(result);
-			return result;
+			return {
+				options: result.data.map((city) => {
+					return {
+						value: `${city.latitude} ${city.longitude}`,
+						label: `${city.name}, ${city.countryCode}`,
+					};
+				}),
+			};
 		} catch (error) {
 			console.error(error);
+			return error;
 		}
+	};
+	const handleOnChange = (searchData) => {
+		setSearch(searchData);
+		onSearchChange(searchData);
 	};
 	return (
 		<AsyncPaginate
