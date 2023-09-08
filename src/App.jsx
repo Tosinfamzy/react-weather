54c3c2b6734304a0f5e31e72d710dfc618a2ca9d
Tosinfamzy <famurewatosin@gmail.com>
2023-09-08 21:53:01 +0100

    complete the forecast feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import './App.css';
 import Search from './components/search/Search';
 import CurrentWeather from './components/weather/currentWeather';
+import Forecast from './components/weather/Forecast';
 
 function App() {
 	const [currentWeather, setCurrentWeather] = useState(null);
+	const [forecastWeather, setForecastWeather] = useState(null);
 	const handleOnSearchChange = async (searchdata) => {
 		const [lat, long] = searchdata.value.split(' ');
 		try {
@@ -13,8 +15,18 @@ function App() {
 					import.meta.env.VITE_WEATHER_API_KEY
 				}&units=metric`
 			);
+			const forecastResponse = await fetch(
+				`${
+					import.meta.env.VITE_FORECAST_API_URL
+				}?lat=${lat}&lon=${long}&appid=${
+					import.meta.env.VITE_WEATHER_API_KEY
+				}&units=metric`
+			);
+			//Should probably use promise.all in the refactor
 			const result = await response.json();
+			const forecastResult = await forecastResponse.json();
 			setCurrentWeather({ city: searchdata.label, ...result });
+			setForecastWeather({ city: searchdata.label, ...forecastResult });
 		} catch (error) {
 			console.error(error);
 			return error;
@@ -24,6 +36,7 @@ function App() {
 		<div className='container'>
 			<Search onSearchChange={handleOnSearchChange} />
 			{currentWeather && <CurrentWeather data={currentWeather} />}
+			{forecastWeather && <Forecast data={forecastWeather} />}
 		</div>
 	);
 }
